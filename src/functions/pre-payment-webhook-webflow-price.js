7dee590f1b28c90112d40c2051963d680309122a
ndvo <nelson@ocastudios.com>
2020-10-04 16:33:24 -0300

    refact(pre-pay-wf): remove unused code

--- src/functions/pre-payment-webhook-webflow-price.js
+++ src/functions/pre-payment-webhook-webflow-price.js
@@ -2,47 +2,19 @@ const Webflow = require('webflow-api');
 const fetch = require('node-fetch');
 
 
-function invalidInput(event) {
-  const keys = Object.keys(event.queryStringParameters);
-  if (keys.length < 3)
-    return "Provide currency, price and product"
-  if (keys.length > 3 && !keys.includes('skuField')) {
-    return "Specify the skuField"
-  }
-  return false;
-}
-
-function getSKUfield(query) {
-  let skuField;
-  const keys = Object.keys(query);
-  if (keys.length === 3) {
-    for (let k of keys) {
-      if (k !== 'currency' && k !== 'price') {
-        skuField = k;
-      }
-    }
-  } else {
-    skuField = query.sku;
-  }
-  return skuField;
-}
-
 function validPrice(price, item, priceField = undefined) {
   if (!priceField) {
     priceField = 'price'
   }
-  console.log('Price',  parseFloat(price) === parseFloat(item[priceField]) );
   return parseFloat(price) === parseFloat(item[priceField]);
 }
 
 function validQuantity(quantity, item, quantityField = undefined) {
-  console.log('Quantity', !quantityField || item[quantityField] >= quantity);
   return !quantityField || item[quantityField] >= quantity;
 }
 
 
 exports.handler = function(event, context, callback) {
-  //console.log("params", event, context, callback);
   const currency = event.queryStringParameters.currency;
   const price = event.queryStringParameters.price;
   const collectionId = event.queryStringParameters.collectionId;
@@ -55,9 +27,7 @@ exports.handler = function(event, context, callback) {
     return callback("Invalid input", {statusCode: 400, body: "Bad request", "Content-Type": "application/json"});
   } else {
     const webflow = getWebflow();
-    webflow.info().then(info => console.log('Info', info))
     webflow.item({collectionId, itemId}).then(item => {
-      console.log(item);
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({
