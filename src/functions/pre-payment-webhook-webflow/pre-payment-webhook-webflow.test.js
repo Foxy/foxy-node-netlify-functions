6076e271aca9c2c5006cd3c1919e881560b641ca
ndvo <nelson@ocastudios.com>
2020-10-06 19:05:57 -0300

    test(pre-payment-webflow): should extract items and identify fields

--- src/functions/pre-payment-webhook-webflow/pre-payment-webhook-webflow.test.js
+++ src/functions/pre-payment-webhook-webflow/pre-payment-webhook-webflow.test.js
@@ -1,13 +1,58 @@
 const rewire = require('rewire');
-const prePayment = rewire('./pre-pament-webhook-webflow-price');
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
 
+const prePayment = rewire('./pre-payment-webhook-webflow');
+const { basicRequest, longCollection } = require('./mock/request');
 
-describe('Verifies the price of an item in a Webflow collection', function() {
+const MockWebFlow = {
+  items({ collectionId }, query = {}) {
+    return {};
+  },
+};
 
-  it('Identifies the price field');
-  it('Identifies the quantity field');
-  it('Retrieves item data from webflow');
-  it('Verifies that price, currency and product are correct');
+prePayment.__set__('getWebflow', () => MockWebFlow);
 
+describe('Verifies the price of an item in a Webflow collection', () => {
+
+  it('Only executes if there is a WEBFLOW_TOKEN set', async function () {
+    function noToken(error, response) {
+      expect(response.ok).to.equal(false);
+      expect(response.details).to.equal('Webflow token not configured.');
+    }
+    function withToken(error, response) {
+      expect(response.details).not.to.equal('Webflow token not configured.');
+    }
+    expect(prePayment.handler(null, null, noToken));
+    prePayment.__set__('process.env.WEBFLOW_TOKEN', 'FOOBAR');
+    expect(prePayment.handler(null, null, withToken));
+  });
+
+  it('Extracts the items from FoxyCart payload', async function() {
+    const extractItems = prePayment.__get__('extractItems');
+    let items = extractItems(basicRequest);
+    expect(items.length).to.equal(2);
+    items = extractItems(longCollection);
+    expect(items.length).to.equal(100);
+  });
+
+  it('Identifies the price, quantity and code fields', async function() {
+    const extractItems = prePayment.__get__('extractItems');
+    const getCustomItemOption = prePayment.__get__('getCustomItemOption');
+    const items = extractItems(longCollection);
+    for (let i of items) {
+      expect(Number(getCustomItemOption(i, 'price'))).to.be.a('number');
+      expect(Number(getCustomItemOption(i, 'quantity'))).to.be.a('number');
+      expect(Number(getCustomItemOption(i, 'inventory'))).to.be.a('number');
+    }
+    expect(items.length).to.equal(100);
+  });
+
+  it('Approves when all items are correct');
+  it('Rejects when any price is incorrect');
+  it('Rejects when provided custom field does not exist');
+  it('Rejects when any quantity over inventory');
+  it('Returns Bad Request when no body is provided');
+  it('Returns Rate limit exceeded when Weflow limit is exceeded');
 
 });
