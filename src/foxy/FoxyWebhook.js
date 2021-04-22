8cd6eea53fd73ae7d469d720eee6e85d58e84e1b
ndvo <nelson@ocastudios.com>
2021-04-22 14:01:17 -0300

    feat(shiptheory): push items from cart to shiptheory

---
+++ src/foxy/FoxyWebhook.js
@@ -0,0 +1,202 @@
+const FoxySdk = require('@foxy.io/sdk');
+const config = require('../../config.js');
+
+
+/**
+ * @typedef {Object} PrepaymentPayload
+ * @property {Object} _links 
+ * @property {PrepaymentEmbedded} _embedded 
+ * @property {string} customer_uri
+ * @property {string} template_set_uri
+ * @property {string} language
+ * @property {string} locale_code
+ * @property {string} customer_ip
+ * @property {string} ip_country
+ * @property {string} session_name
+ * @property {string} session_id
+ * @property {string} total_item_price
+ * @property {string} total_tax
+ * @property {string} total_shipping
+ * @property {string} total_future_shipping
+ * @property {string} total_order
+ * @property {string} date_created
+ * @property {string} date_modified
+ */
+
+/**
+ * @typedef {Object} PrepaymentEmbedded
+ * @property {Array<PrepaymentItem>} fx:items
+ * @property {Array} fx:discounts
+ * @property {Array} fx:custom_fields
+ * @property {Array} fx:shipmet
+ * @property {Array} fx:customer
+ */
+
+/**
+ * @typedef {Object} PrepaymentItem
+ * @property {string} item_category_uri
+ * @property {string} name
+ * @property {string} price
+ * @property {string} quantity
+ * @property {string} quantity_min
+ * @property {string} quantity_max
+ * @property {string} weight
+ * @property {string} code
+ * @property {string} parent_code
+ * @property {string} discount_name
+ * @property {string} discount_type
+ * @property {string} discount_details
+ * @property {string} subscription_frequency
+ * @property {string} subscription_start_date
+ * @property {string} subscription_next_transaction_date
+ * @property {string} subscription_end_date
+ * @property {string} is_future_line_item
+ * @property {string} shipto
+ * @property {string} url
+ * @property {string} image
+ * @property {string} length
+ * @property {string} width
+ * @property {string} height
+ * @property {string} expires
+ * @property {string} date_created
+ * @property {string} date_modified
+ */
+
+/**
+ * Retrieves the items from the payload
+ *
+ * @param {PrepaymentPayload} payload to provide the items.
+ * @returns {Array<PrepaymentItem>} an array of items from this payload
+ */
+function getItems(payload) {
+  try {
+    return payload._embedded['fx:items'] || [];
+  } catch (e) {
+    return [];
+  }
+}
+
+/**
+ * Builds a response as expected by the prepayment webhook.
+ *
+ * @param {string} details about the error, if it happened.
+ * @param {number} code the HTTP status code
+ * @returns {{body: string, statusCode: number}} a string to be used as the body of the response.
+ */
+function response(details="", code=200) {
+  if (code !== 200 && (!details || details.match(/^\s*$/))) {
+    throw new Error("An error response needs to specify details.");
+  }
+  return {
+    body: JSON.stringify({
+      details: details || "",
+      ok: details === ""
+    }),
+    statusCode: code
+  }
+}
+
+/**
+ * Creates a details message about insufficient inventory.
+ *
+ * @param {Array} pairs with insufficient inventory.
+ * @returns {string} a configurable message.
+ */
+function messageInsufficientInventory(pairs) {
+  if (!pairs.length) return '';
+  const message = config.datastore.error.insufficientInventory ||
+    'Insufficient inventory for these items';
+  return message + ' ' + pairs
+    .map(p => `${p[1].name}: only ${p[1].inventory} available`).join(';')
+}
+
+/**
+ * Creates a details message about invalid price.
+ *
+ * @param {Array} pairs with invalid price.
+ * @returns {string} a configurable message.
+ */
+function messagePriceMismatch(pairs) {
+  if (!pairs.length) return '';
+  const message = config.datastore.error.priceMismatch ||
+    'Prices do not match:';
+  return message + ' ' + pairs
+    .map(p => p[0].name)
+    .join(', ');
+}
+
+/**
+ * Verifies a Foxy Signature in a Webhook.
+ *
+ * @param {string} payload received in the Foxy Webhook request.
+ * @param {string} signature received in the Foxy Webhook request.
+ * @param {string} key to be used to verify the signature.
+ * @returns {boolean} the signature is valid
+ */
+function validSignature(payload, signature, key) {
+  try {
+    return FoxySdk.Backend.verifyWebhookSignature({ key, payload, signature });
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+/**
+ * Verifies the signature of a Foxy Webhook Request.
+ *
+ * @param {Object} req the request with the signature to be verified
+ * @returns {boolean} the signature is valid
+ */
+function verifyWebhookSignature(req) {
+  const foxyEvent = req.headers['foxy-webhook-event'];
+  const signature = req.headers['foxy-webhook-signature'];
+  if (foxyEvent === 'validation/payment') {
+    if (!signature) {
+      return true;
+    }
+  }
+  const key = config.foxy.webhook.encryptionKey;
+  const payload = req.body;
+  return validSignature(payload, signature, key);
+}
+
+/**
+ * Validates a Foxy request.
+ *
+ * It must be a Signed POST request with content-type application/json.
+ *
+ * @param {Request} requestEvent to be evaluated as valid.
+ * @returns {string|boolean} the error with this request.
+ */
+function validFoxyRequest(requestEvent) {
+  let err = false;
+  if (!requestEvent) {
+    err = 'Request Event does not Exist';
+  } else if (!requestEvent.body) {
+    err = 'Empty request.';
+  } else if (!requestEvent.httpMethod || requestEvent.httpMethod !== 'POST') {
+    err = 'Method not allowed';
+  } else if (requestEvent.headers['content-type'] !== 'application/json') {
+    err = 'Content type should be application/json';
+  } else if (!verifyWebhookSignature(requestEvent)) {
+    err = 'Forbidden';
+  }
+  try {
+    JSON.parse(requestEvent.body);
+  } catch (e) {
+    err = 'Payload is not valid JSON.';
+  }
+  return err;
+}
+
+module.exports = {
+  getItems,
+  messageInsufficientInventory,
+  messagePriceMismatch,
+  response,
+  validFoxyRequest,
+  validSignature,
+  verifyWebhookSignature,
+}
+
---
+++ src/functions/shiptheory/index.js
@@ -0,0 +1,62 @@
+const FoxyWebhook= require('../../foxy/FoxyWebhook.js');
+const webhook = require('./webhook.js');
+const config = require("../../../config.js");
+
+/**
+ * @callback requestCallback
+ * @param {}
+ */
+
+/**
+ * Receives the request, process it and invokes the callback.
+ *
+ * @param {Object} requestEvent the event built by Netlify upon receiving the request.
+ * @returns {Promise<{statusCode: number, body: string}>} the response object
+ */
+async function handleRequest(requestEvent) {
+  // Validation
+  if (!validation.configuration.validate()) {
+    return validation.configuration.response();
+  }
+  const inputError = validation.input.getError(requestEvent); 
+  if (inputError) {
+    return validation.input.response(inputError);
+  }
+  const response = webhook.transactionCreated(JSON.parse(requestEvent.body));
+  return response;
+}
+
+/**
+ * @typedef {Object} Validation
+ * @property {Function} response a function that builds the response
+ * @property {Function} validate a function that is used to validate
+ */
+
+// The validation object is used to aggregate validation functions
+// and responses.
+// @type {Object<string, Validation>}
+const validation = {
+  configuration: {
+    response: () => webhook.response(
+      "Service Unavailable. Check the webhook error logs.",
+      503),
+    validate: () => {
+      if (!process.env["FOXY_SHIPTHEORY_EMAIL"]) {
+        console.error("FOXY_SHIPTHEORY_EMAIL is not configured");
+      }
+      if (!process.env["FOXY_SHIPTHEORY_PASSWORD"]) {
+        console.error("FOXY_SHIPTHEORY_PASSWORD is not configured");
+      }
+      if (!process.env["FOXY_WEBHOOK_ENCRYPTION_KEY"]) {
+        console.error("FOXY_WEBHOOK_ENCRYPTION_KEY is not configured");
+      }
+      return process.env["FOXY_SHIPTHEORY_EMAIL"] && process.env["FOXY_SHIPTHEORY_PASSWORD"] && process.env["FOXY_WEBHOOK_ENCRYPTION_KEY"];
+    }
+  },
+  input: {
+    getError: FoxyWebhook.validFoxyRequest,
+    response: (message) => webhook.response(message, 400),
+  }
+};
+
+exports.handler = handleRequest;
